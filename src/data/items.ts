46ee01b72920c4a03064672c62fa6af534dbbd5d
Adalberto Maia <btowmaia@gmail.com>
2022-06-14 17:18:27 -0300

    aew

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  {
+    date: new Date(2022,5,5),
+    category: 'food',
+    title: 'McDonalds',
+    value: 32.12
+  },
+  {
+    date: new Date(2022,5,16),
+    category: 'food',
+    title: 'Burguer King',
+    value: 28.00
+  },
+  {
+    date: new Date(2022,5,20),
+    category: 'rent',
+    title: 'Aluguel',
+    value: 2300
+  },
+  {
+    date: new Date(2022,6,5),
+    category: 'salary',
+    title: 'Salario',
+    value: 1100
+  },
+]
